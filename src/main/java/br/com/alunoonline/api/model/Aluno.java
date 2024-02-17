23d506da3ad058bb571cc5e2e89e6784f0f06775
rfujioka <rodrigofujioka@gmail.com>
2024-02-17 09:09:21 -0300

    INclusão de vaidators e exception handlers

--- src/main/java/br/com/alunoonline/api/model/Aluno.java
+++ src/main/java/br/com/alunoonline/api/model/Aluno.java
@@ -4,12 +4,14 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
+@Builder
 @Entity
 public class Aluno {
 
